a6979d8d4f32b0b644c7216bbd0dcdcb9453065d
Aceinet <105454492+Aceinet@users.noreply.github.com>
2022-05-13 12:35:00 +0300

    Add files via upload

--- fakeroot.py
+++ fakeroot.py
@@ -7,9 +7,8 @@ print("""
  | |__ __ _| | _____| |__) |___   ___ | |_ 
  |  __/ _` | |/ / _ \\  _  // _ \ / _ \\| __|
  | | | (_| |   <  __/ | \\ \\ (_) | (_) | |_ 
- |_|  \\__,_|_|\\_\\___|_|  \\_\\___/ \\___/ \__|                                                                                                      
-""")                 
-print('---Created by Aceinet---\nYou are now root')
+ |_|  \\__,_|_|\\_\\___|_|  \\_\\___/ \\___/ \__|""")                 
+print('\n---Created by Aceinet---\nTo install packages first exit the root\n-----------\nRoot switched on ✓')
 
 os.system('proot -0 -w ~ $PREFIX/bin/bash')
 os.system('clear')
